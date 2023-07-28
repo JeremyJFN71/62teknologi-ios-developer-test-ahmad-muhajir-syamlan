863b119e8c320f7cf7d9e2a888d522f28bdb103f
Husain Fadhilah <itdev.support26@tokiomarine-life.co.id>
2023-07-28 13:23:31 +0700

    Initial Commit

---
+++ RandomApp/RandomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomAppApp.swift
+//  RandomApp
+//
+//  Created by Tokio Marine on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
